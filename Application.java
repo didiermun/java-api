95be0cd6f7f86b39f114b95a987b3b87de8cb8d5
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-31 16:25:22 +0200

    initial commit

--- Application.java
+++ Application.java
@@ -1,18 +1,26 @@
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import com.sun.net.httpserver.HttpServer;
+
 
 class Application {
 
     public static void main(String[] args) throws IOException {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        System.out.println("Server started");
         server.createContext("/api/hello", (exchange -> {
 
             if ("GET".equals(exchange.getRequestMethod())) {
-                String respText = "Hello!";
+                String noNameText = "Anonymous";
+                String respText = String.format("Hello %s!", noNameText);
                 exchange.sendResponseHeaders(200, respText.getBytes().length);
                 OutputStream output = exchange.getResponseBody();
                 output.write(respText.getBytes());
@@ -25,5 +33,7 @@ class Application {
         server.setExecutor(null); // creates a default executor
         server.start();
     }
+
+
 
 }
